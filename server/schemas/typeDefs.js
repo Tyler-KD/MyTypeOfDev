c9fd331a8ed1c1aa81fbbd86197b6164501cda7f
Jpalomo <Jpalomo92@gmail.com>
2024-04-25 21:47:48 -0500

    put in the or method in the login mutation - need to implement a type for profile

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -39,7 +39,7 @@ const typeDefs = `
 
   type Mutation {
     addUser(firstName: String!, lastName: String!, username: String!, email: String!, password: String!): Auth
-    login(email: String!, password: String!): Auth
+    login(email: String! || username: String!, password: String!): Auth
     addPost(postText: String!): Post
     addComment(commentId: ID!, commentText: String!): Post
     removePost(postId: ID!): Post
