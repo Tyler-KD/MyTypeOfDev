8b5dfa5154d6154e580c9681c274315f83e93fc6
unknown <tyler.kd.knapp@gmail.com>
2024-04-25 21:47:21 -0500

    Added Navbar and SignupForm components

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,20 +1,55 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Navbar, Nav, Container, Modal, Tab } from 'react-bootstrap';
+import { Navbar, Nav, Container, Modal, Tab, Button, Form, FormGroup } from 'react-bootstrap';
+import LoginForm from './LoginForm';
+import SignupForm from './SignupForm';
 
 const AppNavbar = () => {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
         <>
-        <Navbar bg='dark' variant='dark' expand='lg'>
-            <Container fluid>
-                <Navbar.Brand as={Link} to='/'>
-                    devHub
-                </Navbar.Brand>
+            <Navbar bg='dark' variant='dark' expand='lg'>
+                <Container fluid>
+                    <Navbar.Brand as={Link} to='/'>
+                        devHub
+                    </Navbar.Brand>
+                    <Nav className='ml-auto'>
+                        <Button variant='primary' onClick={handleShow}>
+                            Login/Signup
+                        </Button>
+                    </Nav>
+                </Container>
+            </Navbar>
 
-            </Container>
+            <Modal show={show} onHide={handleClose}>
+                <Tab.Container id="left-tabs-example" defaultActiveKey="login">
+                    <Nav variant="pills">
+                        <Nav.Item>
+                            <Nav.Link eventKey="login">Login</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link eventKey="signup">Sign Up</Nav.Link>
+                        </Nav.Item>
+                    </Nav>
+                    <Tab.Content>
+                        <Tab.Pane eventKey="login">
+                            <LoginForm />
+                        </Tab.Pane>
 
-        </Navbar>
-        
+                        <Tab.Pane eventKey="signup">
+                            <SignupForm />
+                        </Tab.Pane>
+                    </Tab.Content>
+
+
+                </Tab.Container>
+
+            </Modal>
+
         </>
     );
 };
