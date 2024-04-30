f74eb6927211231de851d96e351851b11c3b22ef
yoboyyash <yashrajbaviskar@gmail.com>
2024-04-30 05:10:57 -0500

    What's a goon to a goblin -lil wayne

--- client/src/pages/CreateProfile.jsx
+++ client/src/pages/CreateProfile.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 const CreateProfile = () => {
     const [about, setAbout] = useState('');
@@ -13,19 +13,20 @@ const CreateProfile = () => {
     };
 
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen">
-            <h1 className="text-4xl font-bold mb-8">Create your Profile</h1>
-            <form className="w-full max-w-sm">
+        <div className="flex flex-col items-center justify-center min-h-scree">
+            <h1 className="text-4xl font-bold mb-8 bg-blue-500">Create your Profile</h1>
+
+            <form className="w-full max-w-sm bg-blue-500">
                 <label className="block mb-4">
                     <span className="text-gray-700">About Me:</span>                    
-                    <textarea className="mt-1 block w-full roundedmd border-gray-300 shadow-sm focus:border-indigo-300 focus:ring-indigo-200 focus:ring-opacity-50"
+                    <textarea className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring-indigo-200 focus:ring-opacity-50"
                      rows="3"
                      value={about} 
                      onChange={handleAboutChange} />
                 </label>
                 <label className="block mb-4">
                     <span className="text-gray-700">Upload Picture:</span>                    
-                    <input className="mt-1 block w-full roundedmd border-gray-300 shadow-sm focus:border-indigo-300 focus:ring-indigo-200 focus:ring-opacity-50"
+                    <input className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring-indigo-200 focus:ring-opacity-50"
                     type="file" 
                     onChange={handleImageUpload} />
                 </label>
