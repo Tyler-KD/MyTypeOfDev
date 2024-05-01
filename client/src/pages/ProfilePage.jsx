00aaa938df6893ca8c0338b88fcbb935c4b2b2c6
yoboyyash <yashrajbaviskar@gmail.com>
2024-05-01 04:56:47 -0500

    Cold wit it, new styles bbs

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -11,15 +11,18 @@ const ProfilePage = () => {
     console.log(data);
 
     return (
-        <div className='flex flex-col items-center justify-center min-h-screen'>
-            <h1 className='text-4xl font-bold mb-8 bg-blue-500'>Your Profile</h1>
-            <div className='w-full max-w-sm bg-blue-500'>
-                <p><strong>First Name:</strong> {data.me.firstName}</p>
-                <p><strong>Last Name:</strong> {data.me.lastName}</p>
-                <p><strong>About Me:</strong> {data.me.about}</p>
-                {data.me.image && <img className='mt-4 rounded' src={data.me.image} alt='Profile' />}
+        <div className='flex flex-col items-center justify-center'>
+            <div className='bg-blue-500'>        
+                <h1 className='text-4xl font-bold mb-8'>Your Profile</h1>
+                <div className='w-full max-w-sm'>
+                    <p><strong>First Name:</strong> {data.me.firstName}</p>
+                    <p><strong>Last Name:</strong> {data.me.lastName}</p>
+                    <p><strong>About Me:</strong> {data.me.about}</p>
+                    {data.me.image && <img className='mt-4 rounded' src={data.me.image} alt='Profile' />}
 
+                </div>
             </div>
+    
             
         </div>
     )
