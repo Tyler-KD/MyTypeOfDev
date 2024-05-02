a616ce1eaa0a4af24cf5e3e59dfab8540a220abc
unknown <tyler.kd.knapp@gmail.com>
2024-05-02 00:30:10 -0500

    Replaced localhost 127.0.0.1 with 0.0.0.0

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require("dotenv").config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/devhub');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/devhub');
 
 module.exports = mongoose.connection;
